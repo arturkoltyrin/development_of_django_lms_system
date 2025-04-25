4ef21a69c7c93d7bd130ebb53276c89b20e748b6
Artur Koltyrin <koltyrinartur@gmail.com>
2025-04-26 01:48:13 +0500

    Introduction to Django and DRF

--- lms/models.py
+++ lms/models.py
@@ -1,3 +1,73 @@
 from django.db import models
 
-# Create your models here.
+
+class Course(models.Model):
+    title = models.CharField(
+        max_length=100,
+        verbose_name="название курса",
+        help_text="укажите название курса",
+    )
+    description = models.TextField(
+        max_length=250,
+        verbose_name="описание курса",
+        help_text="опишите курс",
+        blank=True,
+        null=True,
+    )
+    preview = models.ImageField(
+        upload_to="lms/previews",
+        verbose_name="превью",
+        blank=True,
+        null=True,
+        help_text="Загрузите превью",
+    )
+
+    class Meta:
+        verbose_name = "Курс"
+        verbose_name_plural = "Курсы"
+
+    def __str__(self):
+        return self.title
+
+
+class Lesson(models.Model):
+    title = models.CharField(
+        max_length=100,
+        verbose_name="название урока",
+        help_text="укажите название урока",
+    )
+    description = models.TextField(
+        max_length=250,
+        verbose_name="описание урока",
+        help_text="опишите урок",
+        blank=True,
+        null=True,
+    )
+    picture = models.ImageField(
+        upload_to="lms/pictures",
+        verbose_name="картинка",
+        blank=True,
+        null=True,
+        help_text="Загрузите картинку",
+    )
+    course = models.ForeignKey(
+        Course,
+        on_delete=models.CASCADE,
+        verbose_name="курс",
+        help_text="выберите курс",
+        related_name="lesson_set",
+    )
+    video_url = models.URLField(
+        max_length=200,
+        blank=True,
+        null=True,
+        verbose_name="ссылка на видео",
+        help_text="загрузите видео",
+    )
+
+    class Meta:
+        verbose_name = "Урок"
+        verbose_name_plural = "Уроки"
+
+    def __str__(self):
+        return self.title
---
+++ lms/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path
+from rest_framework.routers import SimpleRouter
+
+from lms.apps import LmsConfig
+from lms.views import (CourseViewSet, HomePageView, LessonCreateApiView,
+                       LessonDestroyApiView, LessonListApiView,
+                       LessonRetrieveApiView, LessonUpdateApiView)
+
+app_name = LmsConfig.name
+
+router = SimpleRouter()
+router.register("courses", CourseViewSet)
+urlpatterns = [
+    path("", HomePageView.as_view(), name="home"),
+    path("lessons/", LessonListApiView.as_view(), name="lessons_list"),
+    path("lessons/<int:pk>", LessonRetrieveApiView.as_view(), name="lessons_retrieve"),
+    path("lessons/create/", LessonCreateApiView.as_view(), name="lessons_create"),
+    path("lessons/<int:pk>/delete/",LessonDestroyApiView.as_view(),name="lessons_delete"),
+    path("lessons/<int:pk>/update/", LessonUpdateApiView.as_view(), name="lessons_update"),
+]
+
+urlpatterns += router.urls
